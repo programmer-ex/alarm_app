12e648f5454544129ca9d9cab54ac2a84589c7ff
NASA <yutorin0116@gmail.com>
2025-07-13 11:36:57 +0900

    時間指定設定追加

--- itunes_alarm.py
+++ itunes_alarm.py
@@ -1,5 +1,6 @@
 import random
 import time
+import datetime
 import subprocess
 
 songs = [
@@ -10,8 +11,17 @@ songs = [
 
 selected_song = random.choice(songs)
 
-print("5秒後にiTunesの曲を再生します...")
-time.sleep(5)
+target_hour = 7
+target_minute = 00
+
+print(f"{target_hour}:{target_minute} にアラームが鳴ります...")
+
+while True:
+    now = datetime.datetime.now()
+    if now.hour == target_hour and now.minute == target_minute:
+        break
+    time.sleep(30)  # 30秒ごとに確認
+
 
 applescript = f'''
 tell application "Music"
